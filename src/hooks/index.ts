df6a6a1bd6a79bd38a1a03932962bd1ce2b4709b
sanfeed <sanfeed@mail.ru>
2024-06-16 23:32:34 +0300

    index fix

--- src/hooks/index.ts
+++ src/hooks/index.ts
@@ -1,6 +1,4 @@
 import useOnClickOutside from './useOnClickOutside'
 import useAuth from './useAuth'
-import useThrottle from './useThrottle'
-import useTimeout from './useTimeout'
 
-export { useAuth, useOnClickOutside, useThrottle, useTimeout }
+export { useAuth, useOnClickOutside }
